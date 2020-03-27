c26222b8487cfd2f68770f9a23d8e4fe4f4d71ea
defektive <defektive@users.noreply.github.com>
2020-03-27 16:07:27 -0600

    blackarch shasum

--- provisioners/install_blackarch.sh
+++ provisioners/install_blackarch.sh
@@ -4,8 +4,10 @@
 cd /tmp
 curl -sO https://blackarch.org/strap.sh
 
+SHASUM=$(curl -s https://blackarch.org/downloads.html | grep -P "SHA1 .+$" | awk '{print $7}')
+
 # The SHA1 sum should match: 9f770789df3b7803105e5fbc19212889674cd503 strap.sh
-sha1sum strap.sh | grep '9f770789df3b7803105e5fbc19212889674cd503' || exit
+sha1sum strap.sh | grep "$SHASUM" || exit
 
 chmod +x strap.sh
 sudo ./strap.sh

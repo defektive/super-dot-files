e1258be6cf6ed86e9aaab58f16f590c7ff7e69cb
defektive <defektive@users.noreply.github.com>
2020-03-30 17:05:30 -0600

    netstuff

--- provisioners/bootstrap.sh
+++ provisioners/bootstrap.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 pacman -Syu --noconfirm
-pacman --noconfirm -S git vim base-devel zsh screenfetch nmap ntp ruby jq tmux gdb ipcalc hydra radare2 go scapy openssh the_silver_searcher unzip bluez bluez-utils blueman
+pacman --noconfirm -S git vim base-devel zsh screenfetch nmap ntp ruby jq tmux gdb ipcalc hydra radare2 go scapy openssh the_silver_searcher unzip bluez bluez-utils blueman netctl wpa_supplicant dhcpcd dialog
